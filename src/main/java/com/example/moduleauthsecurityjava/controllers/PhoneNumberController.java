1ed788303d1b5147a08d3ef87a2d1b862ab34195
andrejz2010 <zacharovandrej1@gmail.com>
2024-09-12 15:37:48 +0100

    feat: added validation in controller

--- src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
+++ src/main/java/com/example/moduleauthsecurityjava/controllers/PhoneNumberController.java
@@ -1,15 +1,46 @@
 package com.example.moduleauthsecurityjava.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.moduleauthsecurityjava.models.PhoneNumberRequest;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class PhoneNumberController {
+
+    private static final int PHONE_NUMBER_LENGTH = 11;
+
     @PostMapping("/phone-number")
-    public String phoneNumberSubmit(@ModelAttribute PhoneNumberRequest request) {
-        System.out.println("\033[96mThe user submitted the phone number: " + request.getPhone() + "\033[0m");
-        return "Successful!";
+    public ResponseEntity<String> phoneNumberSubmit(@ModelAttribute PhoneNumberRequest request) {
+        if (isValidPhoneNumber(request.getPhone())) {
+            return ResponseEntity.ok("Successful!");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid phone number format");
+        }
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.isEmpty()) {
+            System.out.println("Empty or null phone number provided");
+            return false;
+        }
+        if (phoneNumber.length() != PHONE_NUMBER_LENGTH) {
+            System.out.println("Invalid length phone number format provided");
+            return false;
+        }
+        if (phoneNumber.matches("[0-9]+")) {
+            return true;
+        }
+        System.out.println("Invalid phone number format provided");
+        return false;
     }
 }
